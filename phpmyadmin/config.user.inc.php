960299a2d64998e1b4189862c6652977f441cb00
Richelly Italo <richelly.italo@convicti.com.br>
2023-08-10 14:03:55 -0300

    chore: keeping phpmyadmin config

---
+++ phpmyadmin/config.user.inc.php
@@ -0,0 +1,9 @@
+<?php
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+
+// Increase the maximum file upload size
+ini_set('upload_max_filesize', '800M');
+ini_set('post_max_size', '128M');
+ini_set('memory_limit', '256M');
+ini_set('max_execution_time', '120');
